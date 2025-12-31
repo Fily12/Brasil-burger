c2dea8d53b95ca7f5df0a9e40ac4b44ceb11bc28
fily <filythiaw0@gmail.com>
2025-12-31 10:18:25 +0000

    Correction DashboardController : redirection /admin vers /admin/dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,12 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin', name: 'admin_')]
 class DashboardController extends AbstractController
 {
-  #[Route('/admin', name: 'admin_dashboard')]
-public function index(): Response
-{
-    return $this->render('admin/dashboard/index.html.twig');
-}
+    #[Route('', name: 'home')]
+    public function home(): Response
+    {
+        // Redirige automatiquement vers le tableau de bord
+        return $this->redirectToRoute('admin_dashboard');
+    }
 
+    #[Route('/dashboard', name: 'dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('admin/dashboard/index.html.twig');
+    }
 }
